255e8b0e845d0c534e3f560ae4787c3f0fb4ebf9
tomygp97 <tomasgp1997@gmail.com>
2024-09-24 12:18:18 -0300

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,10 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html className="scroll-smooth" lang="en">
+      <head>
+        <title>Tomi Portfolio</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={`flex flex-col min-h-screen ${inter.className}`}>
         <div>
           <Header />
